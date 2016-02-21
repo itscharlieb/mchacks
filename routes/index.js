89a9d8820451df6d96c0a2bbf5d68f09eeebc4d6
Geoffrey Long <Geoffrey.Long@mail.mcgill.ca>
2016-02-21 10:52:05 -0500

    Fix logic for search additions

--- routes/index.js
+++ routes/index.js
@@ -120,6 +120,7 @@ router.post('/playlist/create', function(req, res){
     }
     else{
       console.log(JSON.stringify(response));
+      playlist_id = response._id;
       res.render('playlist', { });
     }
   });
