99edfd1b9acd648b5092cc770da12949814208dd
Geoffrey Long <Geoffrey.Long@mail.mcgill.ca>
2016-02-21 18:13:19 -0500

    Change the search population logic

--- public/javascripts/playlist.js
+++ public/javascripts/playlist.js
@@ -65,6 +65,117 @@ function stopVideo() {
   player.stopVideo();
 }
 
+
+function populateListElements(data, isSearch){
+  // isSearch is a boolean which is true if the item is a search item
+  // Data is a json type argument
+  // [{
+  //    name: String for the name of the song
+  //    id: String for the id of the song (in youtube)
+  //    _id: (optional) String for the id of the object in Mongodb
+  //    votes: (optional) An number for the number of votes
+  // }]
+  //
+  // Want this to work for populating the page and for the search elms
+  // So the page population ones will have a votes and the song _id
+  
+  // Would it be possible to create a long string of html then sprintf it?
+  // It probably would be, but would it be more efficient?
+
+
+  // Encapsulating div
+  var newContainer = $(document.createElement('div'));
+  newContainer.addClass('collection z-depth-1');
+
+  if(!isSearch){
+    newContainer = $('#songItems');
+  }
+
+
+  $.each(data, function(i, item) {
+    var elm = null;
+
+    // The data is different depending on the source
+    if (isSearch){
+      var name = item.snippet.title;
+      var yid = item.id.videoId;
+      console.log(item.snippet)
+    
+      elm = createListElement(name, yid, null, null);
+    }
+    else{
+      var item = item.items;
+
+      var name = item.get('name');
+      var yid = item.get('id');
+      var _id = item.get('_id');
+      var votes = item.get('votes');
+
+      // This will be the video that should be played
+      // TODO ill probably have some other logic for this (socketry)
+      if (i == 0){
+        var videoContainer = '<div class="col s8">'
+          + ' <div class="center-align" id="player-container">'
+          + ' <div id="player", data-vid_id="' + yid + '">'
+          + ' </div> </div> </div>'
+        $('body').append(videoContainer);
+        
+        // This is the same as a 'continue' statement
+        return true;
+      }
+      else{
+        elm = createListElement(name, yid, _id, votes);
+      }
+    }
+
+    // Add the element to the container
+    newContainer.append(elm);
+
+    // Search needs to be appended to the dom, 
+    // non-search does not (newContainer is on dom)
+    if (isSearch){
+      $('#searchBox').append(newContainer);
+    }
+  });
+}
+
+function createListElement(name, yid, _id, votes){
+  // The list element
+  var elm = $(document.createElement('li'));
+  elm.addClass('collection-item valign-wrapper');
+    
+  if (_id) elm.attr('id', _id);
+
+
+  // create and add the thumbnail object
+  var thumbnail = '<div class="col s4">'
+    + '<img id="thumbnail" class="responsive-img" src="http://img.youtube.com/vi/'
+    + yid + '/default.jpg"></div>';
+  console.log(thumbnail);
+  elm.append(thumbnail);
+
+  // create and add the song name label
+  var songname = '<div class="col s4"> <p id="songname"> ' + name + ' </p> </div>';
+  elm.append(songname);
+
+  // add in the buttons and votes to non-search elements
+  if (_id){
+    var votes = '<div class="col s4"> <p id="vote_val"> ' + votes + '</p> </div>';
+    elm.append(votes);
+
+    var ratecontainer = '<div class="col s2">'
+      + ' <div class="row"> <button class="btn-floating green darken-4 vote"'
+      + ' id="like" data-song_id="' + _id + '">'
+      + ' <i class="material-icons thumb_up"> </i> </button> </div>'
+      + ' <div class="col s2"> <button class="btn-floating red darken-4 vote"'
+      + ' id="dislike" data-song_id="' + _id + '">'
+      + ' <i class="material-icons thumb_down"> </i> </button> </div> </div>'
+    elm.append(ratecontainer);
+  }
+  
+  return elm;
+}
+
 $( document ).ready(function() {
   socket.on("init", function(song_id){
     console.log("socket received init for [" + song_id + "]");
@@ -138,22 +249,13 @@ $( document ).ready(function() {
       var queryString = "https://www.googleapis.com/youtube/v3/search?part=id%2Csnippet"
       queryString += "&q=" + query + "&key=" + api_key;
       console.log(queryString);
+
       $.get( queryString, function( response ) {
-        // TODO should probably do this as all strings like the newContainer
-        // Best to make this scrollable instead of the entire card
-        var newContainer = $('<div class="collection z-depth-1">')
-        var templateElm = $('<li class="collection-item valign-wrapper">'
-          + '<div class="col s4"> <img id="thumbnail" class="responsive-img"></div>'
-          + '<div class="col s4"> <p id="songName"> </p> </div>'
-          + '</li>');
         var response = response.items;
+        populateListElements(response, true);
 
-
+        /*
         $.each(response, function(i, item) {
-          var newElm = templateElm.clone();
-          newElm.data('vidId', item.id.videoId);
-          newElm.find('#thumbnail').attr('src', item.snippet.thumbnails.default.url);
-          newElm.find('#songName').text(item.snippet.title);
           newElm.click(function(){
             var data = {};
             data.songId = $(this).data('vidId');
@@ -182,7 +284,7 @@ $( document ).ready(function() {
         });
 
         newContainer.append("</div>");
-        $('#searchBox').append(newContainer);
+        */
       });
 
       return false;
